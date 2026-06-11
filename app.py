0da5fa301112a1f2e8a25862b1d3859b7b2bcac5
R Shruti Rao <shrutirao29@gmail.com>
2026-06-11 19:18:27 -0400

    Add path configuration to include src directory

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from pathlib import Path
+import sys
+
+
+ROOT = Path(__file__).resolve().parent
+SRC_DIR = ROOT / "src"
+if str(SRC_DIR) not in sys.path:
+    sys.path.insert(0, str(SRC_DIR))
+
 from src.app import main
 
 
